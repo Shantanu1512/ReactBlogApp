9bfd59baf21b8fb7e5ddf80523a07b019a3987b1
Shantanu <moreshantanu09@gmail.com>
2025-07-31 10:58:40 +0530

    store and slices configured for state mgmt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import conf from './conf/conf.js';
-
+import authService from './services/appwrite/auth.js';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice.js';
+import { Footer, Header } from './components/index.js';
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrenctUser()
+      .then((response) => {
+        if (response) {
+          dispatch(login({ response }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(setLoading(false));
+  }, []);
   console.log(conf.appwriteUrl);
-  return (
-    <>
-      <h1>Hello from blog app</h1>
-    </>
+
+  return !loading ? (
+    <div className="h-screen w-screen text-center content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading ....</div>
   );
 }
 
