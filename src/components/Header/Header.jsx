eb7114ada5b54777fb8bb46af21b7803f9e169be
Shantanu <moreshantanu09@gmail.com>
2025-07-31 20:12:20 +0530

    components created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { LogoutBtn, Logo, Container } from '../index';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItem = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true,
+    },
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus,
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus,
+    },
+    {
+      name: 'All Posts',
+      slug: '/all-posts',
+      active: authStatus,
+    },
+    {
+      name: 'Add Post',
+      slug: '/add-post',
+      active: authStatus,
+    },
+  ];
   return (
-    <div>Header</div>
-  )
+    <header>
+      <Container>
+        <nav>
+          <div>
+            <Link>
+              <Logo />
+            </Link>
+          </div>
+          <ul>
+            {navItem.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
